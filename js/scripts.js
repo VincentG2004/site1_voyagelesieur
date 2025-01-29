d2e349f1b7688b62f56c1df56c57e4b5f601d64f
VincentG2004 <vincent.gaudet.bernier@gmail.com>
2025-01-29 14:49:11 -0500

    Javascript fait

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const btnPlus = document.querySelector('.js-plus');
+const colTransport = document.querySelector(".conteneur-transport");
+
+btnPlus.addEventListener("click", ouvreFerme);
+
+function ouvreFerme() {
+    colTransport.classList.toggle("invisible");
+}
